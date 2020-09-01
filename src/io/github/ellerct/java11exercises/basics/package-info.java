503576d5a3756d88c2faf932bc582fae6e6160ef
Chris Eller <cteller@bsu.edu>
2020-09-01 17:44:18 -0400

    Create package info documentation

---
+++ src/io/github/ellerct/java11exercises/basics/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains some non-robust basic exercises for Java 11
+ * <p>
+ *     While the code contained within is not especially robust with a lack of exception handling,
+ *     it follows the general expectations of a 'basic' program. Assuming inputs, fair-weather cases,
+ *     things like that.
+ * </p>
+ * @author Christopher Eller
+ */
+package io.github.ellerct.java11exercises.basics;
